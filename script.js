514431fb0e4b0486eacdb0f9abe5c8d01902db23
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-01-11 00:09:22 +0200

    lecture: Functions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -8,3 +8,28 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
+*/
+
+//Functions
+
+function logger() {
+   console.log('My name is Dima');
+}
+
+// calling / runing / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+   return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
